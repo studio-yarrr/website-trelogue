a31b5c614dc7c3e17ef65225cdbc7f532db5664f
KonstantinVolodko <faker2000311@gmail.com>
2023-11-14 12:03:12 +0300

    all pages

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -10675,14 +10675,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
   let marquee = document.querySelector('.marquee');
   let marqueeText = document.querySelector('.marquee__text');
-  let textWidth = marqueeText.offsetWidth;
+  if (marqueeText) {
+    let textWidth = marqueeText.offsetWidth;
 
-  let tl = gsap.timeline({
-    repeat: -1,
-    defaults: { ease: 'linear' }
-  });
+    let tl = gsap.timeline({
+      repeat: -1,
+      defaults: { ease: 'linear' }
+    });
+  
+  
+    tl.to('.marquee', { x: -textWidth, duration: 20, repeat: -1 });
+  }
+  
 
-  tl.to('.marquee', { x: -textWidth, duration: 20, repeat: -1 });
 
   let swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
@@ -10704,10 +10709,38 @@ document.addEventListener("DOMContentLoaded", () => {
 
   ulElement.classList.remove('flip2', 'flip3', 'flip4', 'flip5');
 
-  switch(liCount) {
-      case 2: ulElement.classList.add('flip2'); break;
-      case 3: ulElement.classList.add('flip3'); break;
-      case 4: ulElement.classList.add('flip4'); break;
-      default: ulElement.classList.add('flip5'); break;
+  switch (liCount) {
+    case 2: ulElement.classList.add('flip2'); break;
+    case 3: ulElement.classList.add('flip3'); break;
+    case 4: ulElement.classList.add('flip4'); break;
+    default: ulElement.classList.add('flip5'); break;
   }
+
+
+  let catalogueTabsContainer = document.querySelector('.catalogue-tabs__container')
+  let catalogueTabsBtns = document.querySelectorAll('.catalogue-tabs button')
+
+  if (catalogueTabsBtns) {
+    catalogueTabsBtns.forEach(e => {
+      e.addEventListener('click', () => {
+        catalogueTabsBtns.forEach(el => el.classList.remove('--active'));
+        e.classList.add('--active');
+        catalogueTabsContainer.classList.add('fading-out');
+    
+        setTimeout(() => {
+          catalogueTabsContainer.innerHTML = e.nextElementSibling.innerHTML;
+          catalogueTabsContainer.classList.remove('fading-out');
+          catalogueTabsContainer.classList.add('fading-in');
+    
+          setTimeout(() => {
+            catalogueTabsContainer.classList.remove('fading-in');
+          }, 300);
+        }, 300);
+      });
+    });
+
+    catalogueTabsBtns[0].click()
+  }
+  
+
 })
